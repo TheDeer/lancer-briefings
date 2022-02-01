189a870d22455ed343fad4ea1fd2f2bbf39b8235
John Gredler <johng.thedeer@gmail.com>
2022-02-01 11:10:14 -0700

    Add mission 002 to the screen

--- src/App.vue
+++ src/App.vue
@@ -89,7 +89,7 @@ export default {
 
   data() {
     return {
-      "mission_slug": "001",
+      "mission_slug": "002",
       "current_md": "",
       "events": "",
       "missions": [
@@ -101,6 +101,11 @@ export default {
         {
           "slug": "001",
           "name": "Bug-Hunt",
+          "status": "success"
+        },
+        {
+          "slug": "002",
+          "name": "Vigilant Gaze",
           "status": "start"
         },
       ],
